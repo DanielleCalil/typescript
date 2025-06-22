d182014ec50b62502affa4bd5fc6f9fb8cbce58d
Danielle Calil <153567808+DanielleCalil@users.noreply.github.com>
2025-06-21 21:52:52 -0300

    feat/intro

---
+++ 1_INTRO/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const firstName = "Danielle";
+const anotherName = 1;
+const x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(firstName);
+// greeting(anotherName)
+// greeting(x)
+// desafio 1
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(2, 2));
+const z = 5;
+const y = 8;
+console.log(soma(z, y));
+// console.log(soma(true, "teste"));
